b0d35fd963f8e5ac81c4fc6cd7cfef4d57322465
Marco Maggi <marco.maggi-ipsu@poste.it>
2020-03-02 10:00:18 +0100

    started test file for mathematics

---
+++ tests/mathematics.el
@@ -0,0 +1,43 @@
+;;; mathematics.el --- dynamic module test
+
+;; Copyright (C) 2020 by Marco Maggi
+
+;; Author: Marco Maggi <mrc.mgg@gmail.com>
+
+;; This program is  free software; you can redistribute  it and/or modify it under the  terms of the
+;; GNU General Public License as published by the  Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+;; even the implied  warranty of MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.   See the GNU
+;; General Public License for more details.
+;;
+;; You should have  received a copy of the  GNU General Public License along with  this program.  If
+;; not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+(require 'mmec)
+
+
+;;;; helpers
+
+
+
+;;;; trigonometric functions
+
+(ert-deftest mmec-float-sin ()
+  "Test the trigonometric sine function."
+  (should (mmec= (sin 0.23) (mmec-sin (mmec-double 0.23))))
+  )
+
+
+;;;; done
+
+(garbage-collect)
+(ert-run-tests-batch-and-exit)
+
+;;; mathematics.el ends here
